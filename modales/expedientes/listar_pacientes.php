a60b133a3da0139154e1ba73f936a349f57b77ea
Jose <josedeodanes99@gmail.com>
2024-06-05 16:17:47 -0600

    Update contra entrega

--- modales/expedientes/listar_pacientes.php
+++ modales/expedientes/listar_pacientes.php
@@ -1,5 +1,5 @@
 <div class="modal fade show" id="modal-pacientes-ref" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog" style="max-width: 55%">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content">
 
             <!-- Modal Header -->
